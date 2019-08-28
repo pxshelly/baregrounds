a4d43c27d5ca10b4d2cacaf9f4bb0ec2e8a387ef
Shelly <pxshelly@gmail.com>
2019-08-28 02:11:50 -0700

    Update sass stylesheet path

--- client/index.jsx
+++ client/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './src/components/App.jsx';
-import './input.scss';
+import '../client/dist/input.scss';
 
 ReactDOM.render(
   <BrowserRouter>
