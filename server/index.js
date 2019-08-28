e3e17c53f4cc58e649806d4351710aed7a870885
Shelly <pxshelly@gmail.com>
2019-08-28 02:12:33 -0700

    Serve static files

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '../client/dist/')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
